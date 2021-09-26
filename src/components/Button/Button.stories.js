cd84c216100a1984474cc9bfe09ea422533c3916
nikhil-isaac <37073895+nikhil-isaac@users.noreply.github.com>
2021-09-26 16:27:04 +0530

    Adding button story

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,36 +1,60 @@
 import React from "react";
 
-import { Button } from "./Button";
+import { Button } from "react-bootstrap";
+
+const variantOptions = [
+  "primary",
+  "secondary",
+  "success",
+  "warning",
+  "danger",
+  "info",
+  "light",
+  "dark",
+  "outline-primary",
+  "outline-secondary",
+  "outline-success",
+  "outline-warning",
+  "outline-danger",
+  "outline-info",
+  "outline-light",
+  "outline-dark",
+];
+
+const sizeOptions = ["lg", "md", "sm"];
 
 export default {
   title: "Components/Button",
   component: Button,
   argTypes: {
-    backgroundColor: { control: "color" },
+    variant: {
+      options: variantOptions,
+      control: { type: "select" },
+    },
+    size: {
+      options: sizeOptions,
+      control: { type: "select" },
+    },
+    active: { control: { type: "boolean" } },
+    disabled: { control: { type: "boolean" } },
   },
 };
 
 const Template = (args) => <Button {...args} />;
 
-export const Primary = Template.bind({});
-Primary.args = {
-  primary: true,
-  label: "Button",
+export const ButtonVariant = Template.bind({});
+ButtonVariant.args = {
+  variant: "primary",
+  children: "Button",
 };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: "Button",
-};
-
-export const Large = Template.bind({});
-Large.args = {
-  size: "large",
-  label: "Button",
+export const Sizes = Template.bind({});
+Sizes.args = {
+  children: "Button",
 };
 
 export const Small = Template.bind({});
 Small.args = {
   size: "small",
-  label: "Button",
+  children: "Button",
 };
