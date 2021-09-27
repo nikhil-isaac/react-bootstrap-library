e553f8994e2498e4a8c1cc9799d0c0ad0192520c
nikhil-isaac <37073895+nikhil-isaac@users.noreply.github.com>
2021-09-27 19:29:27 +0530

    adding card

---
+++ src/components/Card/Card.stories.js
@@ -0,0 +1,153 @@
+import React from "react";
+
+import { Card, Button, ListGroup, ListGroupItem } from "react-bootstrap";
+
+export default {
+  title: "Components/Card",
+  component: Card,
+};
+
+export const Example = () => (
+  <Card style={{ width: "18rem" }}>
+    <Card.Img variant="top" src="holder.js/100px180" />
+    <Card.Body>
+      <Card.Title>Card Title</Card.Title>
+      <Card.Text>
+        Some quick example text to build on the card title and make up the bulk
+        of the card's content.
+      </Card.Text>
+      <Button variant="primary">Go somewhere</Button>
+    </Card.Body>
+  </Card>
+);
+
+export const TitleTextLink = () => (
+  <Card style={{ width: "18rem" }}>
+    <Card.Body>
+      <Card.Title>Card Title</Card.Title>
+      <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+      <Card.Text>
+        Some quick example text to build on the card title and make up the bulk
+        of the card's content.
+      </Card.Text>
+      <Card.Link href="#">Card Link</Card.Link>
+      <Card.Link href="#">Another Link</Card.Link>
+    </Card.Body>
+  </Card>
+);
+
+export const ListGroups = () => (
+  <Card style={{ width: "18rem" }}>
+    <Card.Header>Featured</Card.Header>
+    <ListGroup variant="flush">
+      <ListGroup.Item>Cras justo odio</ListGroup.Item>
+      <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+      <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+    </ListGroup>
+  </Card>
+);
+
+export const KitchenSink = () => (
+  <Card style={{ width: "18rem" }}>
+    <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+    <Card.Body>
+      <Card.Title>Card Title</Card.Title>
+      <Card.Text>
+        Some quick example text to build on the card title and make up the bulk
+        of the card's content.
+      </Card.Text>
+    </Card.Body>
+    <ListGroup className="list-group-flush">
+      <ListGroupItem>Cras justo odio</ListGroupItem>
+      <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+      <ListGroupItem>Vestibulum at eros</ListGroupItem>
+    </ListGroup>
+    <Card.Body>
+      <Card.Link href="#">Card Link</Card.Link>
+      <Card.Link href="#">Another Link</Card.Link>
+    </Card.Body>
+  </Card>
+);
+
+export const HeaderBody = () => (
+  <Card>
+    <Card.Header>Featured</Card.Header>
+    <Card.Body>
+      <Card.Title>Special title treatment</Card.Title>
+      <Card.Text>
+        With supporting text below as a natural lead-in to additional content.
+      </Card.Text>
+      <Button variant="primary">Go somewhere</Button>
+    </Card.Body>
+  </Card>
+);
+
+export const Quote = () => (
+  <Card>
+    <Card.Header>Quote</Card.Header>
+    <Card.Body>
+      <blockquote className="blockquote mb-0">
+        <p>
+          {" "}
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+          posuere erat a ante.{" "}
+        </p>
+        <footer className="blockquote-footer">
+          Someone famous in <cite title="Source Title">Source Title</cite>
+        </footer>
+      </blockquote>
+    </Card.Body>
+  </Card>
+);
+
+export const HeaderBodyFooter = () => (
+  <Card className="text-center">
+    <Card.Header>Featured</Card.Header>
+    <Card.Body>
+      <Card.Title>Special title treatment</Card.Title>
+      <Card.Text>
+        With supporting text below as a natural lead-in to additional content.
+      </Card.Text>
+      <Button variant="primary">Go somewhere</Button>
+    </Card.Body>
+    <Card.Footer className="text-muted">2 days ago</Card.Footer>
+  </Card>
+);
+
+export const ImageCaps = () => (
+  <>
+    <Card>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </Card.Body>
+    </Card>
+    <br />
+    <Card>
+      <Card.Body>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </Card.Body>
+      <Card.Img variant="bottom" src="holder.js/100px180" />
+    </Card>
+  </>
+);
+
+export const ImageOverlays = () => (
+  <Card className="bg-dark text-white">
+    <Card.Img src="holder.js/100px270" alt="Card image" />
+    <Card.ImgOverlay>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This content is a little bit longer.
+      </Card.Text>
+      <Card.Text>Last updated 3 mins ago</Card.Text>
+    </Card.ImgOverlay>
+  </Card>
+);
